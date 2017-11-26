75c72c5ad632d356fb24b00bd9eec1095c9330e0
Paul Ward <asmodai@gmail.com>
2017-11-26 00:37:49 +0000

    [Cxx11]: Eat own dog food and like it.
    This is a branch for the C++11 port of verbuild. Eventually it will replace the Qt version. Oh, when I say 'C++11', I mean 'C++14'.

--- src/verbuild/version.hpp
+++ src/verbuild/version.hpp
@@ -17,7 +17,7 @@
 
 #define VERSION_BASE_YEAR  2017
 #define VERSION_DATE       "2017-Nov-26"
-#define VERSION_TIME       "00:27:47"
-#define VERSION_STRING     "2.0.601.0"
+#define VERSION_TIME       "00:36:36"
+#define VERSION_STRING     "2.0.602.0"
 
 #endif // !__VersionInfo_Header__
